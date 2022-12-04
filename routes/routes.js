31239f5c339115d064bb4908052fefe542506c2b
king <kingsharpn@gmail.com>
2022-12-04 10:03:19 +0000

    hi

--- routes/routes.js
+++ routes/routes.js
@@ -32,46 +32,10 @@ router.get('/getAll', (req, res) => {
      Model.find()
     .then((result) =>{
         res.send(result)
+        return result
     })
     .catch((err) =>{
         console.log(err);
     })
 })
 
-//Get by ID Method
-router.get('/getOne/:id', (req, res) => {
-    res.send('Get by ID API')
-})
-
-//Update by ID Method
-router.patch('/update/:id', async (req, res) => {
-   try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
-
-        const result = await Model.findByIdAndUpdate(
-            id, updatedData, options
-        )
-
-        res.send(result)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
-
-//Delete by ID Method
-router.delete('/delete/:id', async (req, res) => {
-    // res.send('Delete by ID API')
-    try {
-        const id = req.params.id;
-        const data = await Model.findByIdAndDelete(id)
-        res.send(`Document with ${data._id} has been deleted..`)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
-
-module.exports = router;
